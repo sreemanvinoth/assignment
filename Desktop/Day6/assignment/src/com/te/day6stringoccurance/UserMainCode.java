dfb25a4a8d7ff0db5ee4c8664e1b4a722b9b6ed1
sreemanvinoth <shrimanvinoth@gmail.com>
2023-03-06 10:10:27 +0530

    day6 assignment

---
+++ Desktop/Day6/assignment/src/com/te/day6stringoccurance/UserMainCode.java
@@ -0,0 +1,15 @@
+package com.te.day6stringoccurance;
+
+public class UserMainCode {
+
+	public static Integer getSubString(String string1, String string2) {
+
+		if(string1.contains(string2)) {
+			
+		}
+		
+		
+		return null;
+	}
+
+}
